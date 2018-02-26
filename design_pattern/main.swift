1415f02c3c9c256382b79b7b56b2a3c374eb1f13
Tomohiro Imaizumi <info@imaizu.me>
2018-02-26 23:53:53 +0900

    Initial Commit

---
+++ design_pattern/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  design_pattern
+//
+//  Created by Tomohiro Imaizumi on 2018/02/26.
+//  Copyright © 2018年 imaizume. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
