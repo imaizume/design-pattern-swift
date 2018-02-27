7842cee53470368be8ab6b36532df863e2d339cf
Tomohiro Imaizumi <info@imaizu.me>
2018-02-28 01:19:06 +0900

    Add code for Chap. 17: Observer Pattern

--- design_pattern/main.swift
+++ design_pattern/main.swift
@@ -8,5 +8,73 @@
 
 import Foundation
 
-print("Hello, World!")
+extension Notification.Name {
+    static let myObserver = Notification.Name("myObserver")
+}
 
+class RandomNumberGenerator {
+}
+
+class Observer {
+    public func addObserver() {
+        let center = NotificationCenter.default
+        center.addObserver(
+            self,
+            selector: #selector(type(of: self).update(notification:)),
+            name: .myObserver,
+            object: nil
+        )
+    }
+
+    public func removeObserver() {
+        let center = NotificationCenter.default
+        center.removeObserver(
+            self,
+            name: .myObserver,
+            object: nil
+        )
+    }
+
+    @objc public func update(notification: Notification) {}
+}
+
+class DigitObserver: Observer {
+    public override func update(notification: Notification) {
+        if let number = notification.userInfo?["number"] as? Int {
+            print("DigitObserver: \(number)")
+            Thread.sleep(forTimeInterval: 0.1)
+        }
+    }
+}
+
+class GraphObserver: Observer {
+    public override func update(notification: Notification) {
+        if let number = notification.userInfo?["number"] as? Int {
+            print("GraphObserver: ", terminator: "")
+            print(String(repeating: "*", count: number))
+            Thread.sleep(forTimeInterval: 0.1)
+        }
+    }
+}
+
+func execute(times: Int) {
+    for _ in 0..<times {
+        NotificationCenter.default.post(
+            name: .myObserver,
+            object: nil,
+            userInfo: ["number": Int(arc4random_uniform(50))]
+        )
+    }
+}
+
+func main() {
+    let observer1 = DigitObserver()
+    let observer2 = GraphObserver()
+    observer1.addObserver()
+    observer2.addObserver()
+    execute(times: 20)
+    observer1.removeObserver()
+    observer2.removeObserver()
+}
+
+main()
